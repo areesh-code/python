834c7368cce4757cea799fff519f1194cf8bda72
Areesh-code <areeshdinani@yahoo.com>
2024-11-07 17:40:24 +1300

    swap.py

---
+++ All Acps Python/Swap.py
@@ -0,0 +1,7 @@
+x=int(input("enter X"))
+y=int(input("enter Y"))
+z=int(input("enter Z"))
+print("The value of x, y and z before swapping is",x,y,z)
+
+x,y,z=y,z,x
+print("The value of x, y and z after swapping is",x,y,z)
