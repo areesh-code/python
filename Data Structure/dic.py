d47ffe4aa3aaad84121ad79b19e75d237c4df4ab
Areesh-code <areeshdinani@yahoo.com>
2024-11-21 18:23:32 +1300

    lists.py

---
+++ Data Structure/dic.py
@@ -0,0 +1,12 @@
+v={"Name": "Areesh", "Year": 8, "School": "Murrays Bay Intermidiete"}
+print(v)
+print(len(v))
+print(type(v))
+v["Year"]= 2024
+print(v)
+v.update({"Address": "278/6 East Coast Road"})
+print(v)
+v.pop("Address")
+print(v)
+v.clear()
+print(v)
---
+++ Data Structure/lists.py
@@ -0,0 +1,25 @@
+s=["apples", 3,4, "pizza", "donuts"]
+print(s)
+print(len(s))
+print(type(s))
+print(s[0])
+print(s[-1])
+s.append("Areesh")
+print(s)
+s.insert(3,"Strawberry")
+print(s)
+s.remove(3)
+print(s)
+s.pop(4)
+print(s)
+b=[1,4,7,8,3,4,7]
+b.sort()
+print(b)
+b.reverse()
+print(b)
+b.clear()
+print(b)
+
+
+
+
