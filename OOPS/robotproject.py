9989ceef3092b5adf80687ce492ea53aadac17e4
Areesh-code <areeshdinani@yahoo.com>
2024-11-28 18:44:51 +1300

    robotproject.py

---
+++ OOPS/robotproject.py
@@ -0,0 +1,16 @@
+class Robot:
+    def __init__(self, name, age, hobby):
+        self.name = name
+        self.age = age
+        self.hobby = hobby
+    
+    def introduce(self):
+        print(f"Hello, I am {self.name}.")
+        print(f"I am {self.age} years old.")
+        print(f"My hobby is {self.hobby}.\n")
+    
+    tom = robot("Tom", 13, "Cricket") 
+jerry = Robot("Jerry", 10, "Basketball")
+
+tom.introduce()
+jerry.introduce()
